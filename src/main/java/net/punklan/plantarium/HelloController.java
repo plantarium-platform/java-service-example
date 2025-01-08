4f8968c9c45d2a4e05bfd5e4fdbcf0c0beade2eb
Aleksandr Kichev <kichev.alexander@gmail.com>
2025-01-08 17:12:37 +0800

    Smarer url processing on a java server

--- src/main/java/net/punklan/plantarium/HelloController.java
+++ src/main/java/net/punklan/plantarium/HelloController.java
@@ -4,15 +4,15 @@ package net.punklan.plantarium;
 import io.micronaut.context.annotation.Value;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-
-@Controller("/hello")
+@Controller("${base-url:/hello}")
 public class HelloController {
 
     private final Long requestDelay;
     private final String instanceId;
 
-    public HelloController(@Value("${request-delay:0}") Long requestDelay,
-                           @Value("${instance-id:}") String instanceId) {
+    public HelloController(
+            @Value("${request-delay:0}") Long requestDelay,
+            @Value("${instance-id:}") String instanceId) {
         this.requestDelay = requestDelay;
         this.instanceId = instanceId;
     }
@@ -23,7 +23,7 @@ public class HelloController {
             try {
                 Thread.sleep(requestDelay);
             } catch (InterruptedException e) {
-                // Handle the exception appropriately
+                Thread.currentThread().interrupt(); // Restore the interrupt flag
             }
         }
 
